d6878bd9050381ae6440798f4e846bde4bd6d6fe
VishwaaY28 <2000163829@hexaware.com>
2026-06-17 16:24:27 +0530

    bulk export

--- apps/client/src/utils/apiBase.ts
+++ apps/client/src/utils/apiBase.ts
@@ -1,2 +1,6 @@
 export const API_BASE =
   `${(import.meta.env.BASE_URL ?? '/').replace(/\/$/, '')}/api`;
+
+export function apiUrl(path: string): string {
+  return `${API_BASE}/${path.replace(/^\//, '')}`;
+}
